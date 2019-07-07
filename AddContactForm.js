7238781d90873ece3acb29035e9fc99fdf46cda7
TammyLaforest <TammyLaforest@gmail.com>
2019-07-07 10:49:58 -0400

    started navigation lesson. Updated tutorial code according to react-navigation documentation

--- AddContactForm.js
+++ AddContactForm.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { Button, KeyboardAvoidingView, StyleSheet, TextInput, View } from 'react-native'
 import PropTypes from 'prop-types'
-import Constants from 'expo-constants'
+// import Constants from 'expo-constants'
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
     input: {
         borderWidth: 1,
         borderColor: 'black',
@@ -14,37 +17,30 @@ const styles = StyleSheet.create({
         paddingVertical: 5,
         borderRadius: 3,
     },
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        paddingTop: Constants.statusBarHeight,
-        justifyContent: 'center'
-    },
-})
+});
+
 export default class AddContactForm extends React.Component {
-
-    static propTypes = {
-        addContact: PropTypes.func
-    }
-
     state = {
         name: '',
         phone: '',
-        isFormValid: false
+        isFormValid: false,
     }
 
     componentDidUpdate(prevProps, prevState) {
-        if (this.state.name !== prevState.name || this.state.phone !== prevState.phone) {
-            this.validateForm()
+        if (
+            this.state.name !== prevState.name ||
+            this.state.phone !== prevState.phone
+        ) {
+            this.validateForm();
         }
     }
 
-    // getHandler = key => val => {
-    //     this.setState({ [key]: val })
-    // }
+    getHandler = key => val => {
+        this.setState({ [key]: val });
+    };
 
-    // handleNameChange = this.getHandler('name')
-    // handlePhoneChange = this.getHandler('phone')
+    //   handleNameChange = this.getHandler('name'); // val => { this.setState({name: val}) }
+    //   handlePhoneChange = this.getHandler('phone');
 
     handleNameChange = name => {
         this.setState({ name })
@@ -57,22 +53,26 @@ export default class AddContactForm extends React.Component {
     }
 
     validateForm = () => {
-        if (+this.state.phone >= 0 && this.state.phone.length === 10
-            && this.state.name.length >= 2) {
-            this.setState({ isFormValid: true })
+
+        const names = this.state.name.split(' ');
+        if (
+            +this.state.phone >= 0 &&
+            this.state.phone.length === 10 &&
+            names.length >= 2 &&
+            names[0] &&
+            names[1]
+        ) {
+            this.setState({ isFormValid: true });
         } else {
-            this.setState({ isFormValid: false })
+            this.setState({ isFormValid: false });
         }
     }
 
     handleSubmit = () => {
-        this.props.onSubmit(this.state)
-        // this.props.onSubmit({ ...this.state })
-        // this.props.onSubmit({ name: this.state.name, phone: this.state.phone })
+        this.props.onSubmit(this.state);
     }
 
     render() {
-
         return (
             <KeyboardAvoidingView behavior="padding" style={styles.container}>
                 <TextInput
@@ -93,3 +93,20 @@ export default class AddContactForm extends React.Component {
         )
     }
 }
+
+
+//Old Code
+
+// export default class AddContactForm extends React.Component {
+
+//     static propTypes = {
+//         addContact: PropTypes.func
+//     }
+
+
+    // handleSubmit = () => {
+    //     // this.props.onSubmit(this.state)
+    //     this.props.onSubmit({ ...this.state })
+    //     // this.props.onSubmit({ name: this.state.name, phone: this.state.phone })
+    // }
+
--- App.js
+++ App.js
@@ -1,6 +1,22 @@
 import React from 'react';
-import { Button, SectionList, StyleSheet, Text, View } from 'react-native';
+import { Button, Platform, SectionList, StatusBar, StyleSheet, Text, View } from 'react-native';
 import Constants from 'expo-constants'
+
+// Added createAppContainer for new version of React-Navigation
+import {
+  createAppContainer,
+  createStackNavigator,
+  createSwitchNavigator,
+  createBottomTabNavigator
+} from "react-navigation";
+
+import Ionicons from "react-native-vector-icons/Ionicons";
+
+import AddContactScreen from "./screens/AddContactScreen";
+import SettingsScreen from "./screens/SettingsScreen";
+import ContactListScreen from "./screens/ContactListScreen";
+import ContactDetailsScreen from "./screens/ContactDetailsScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 import contacts, { compareNames } from './contacts'
 import Row from './Row'
@@ -15,53 +31,127 @@ const styles = StyleSheet.create({
   },
 });
 
+
+const MainStack = createStackNavigator(
+  {
+    ContactList: ContactListScreen,
+    ContactDetails: ContactDetailsScreen,
+    AddContact: AddContactScreen
+  },
+  {
+    initialRouteName: "ContactList",
+    navigationOptions: {
+      headerTintColor: "#a41034",
+      headerStyle: {
+        backgroundColor: "#fff"
+      }
+    }
+  }
+);
+
+MainStack.navigationOptions = {
+  tabBarIcon: ({ focused, tintColor }) => (
+    <Ionicons
+      name={`ios-contacts${focused ? "" : "-outline"}`}
+      size={25}
+      color={tintColor}
+    />
+  )
+};
+
+const MainTabs = createBottomTabNavigator(
+  {
+    Contacts: MainStack,
+    Settings: SettingsScreen
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: "#a41034"
+    }
+  }
+);
+
+const AppNavigator = createSwitchNavigator({
+  Login: LoginScreen,
+  Main: MainTabs
+});
+
+// App Container required for V3
+const AppContainer = createAppContainer(AppNavigator)
+
 export default class App extends React.Component {
   state = {
-    showContacts: true,
-    showForm: false,
-    contacts: contacts
-  }
+    contacts
+  };
+
   addContact = newContact => {
-    this.setState(prevState => ({ showForm: false, contacts: [...prevState.contacts, newContact] }))
-  }
-  toggleContacts = () => {
-    this.setState(prevState => ({ showContacts: !prevState.showContacts }))
-  }
-
-
-  toggleForm = () => {
-    this.setState(prevState => ({ showForm: !prevState.showForm }))
-  }
-
-
-  showForm = () => {
-    this.setState({ showForm: true })
-  }
-
-  // Fix array so that props change, so sort reloads page
-  sort = () => {
     this.setState(prevState => ({
-      contacts: [...prevState.contacts].sort(compareNames)
-    }))
-  }
+      contacts: [...prevState.contacts, newContact]
+    }));
+  };
 
+  // Pass AppNavigator props to AppContainer instead
   render() {
-    // console.error('full page alert')
-
-    if (this.state.showForm) return <AddContactForm onSubmit={this.addContact} />
     return (
-      <View style={styles.container}>
 
-        <Button title="Toggle Contacts" onPress={this.toggleContacts} />
+      <AppContainer
+        screenProps={{
+          contacts: this.state.contacts,
+          addContact: this.addContact
+        }}
+      />
 
-        <Button title="Add Contact" onPress={this.showForm} />
-
-        {this.state.showContacts && <ContactsList contacts={this.state.contacts} />}
-      </View>
-    )
-
-
+    );
   }
 }
 
 
+// Old Code Below
+
+
+// export default class App extends React.Component {
+//   state = {
+//     showContacts: true,
+//     showForm: false,
+//     contacts: contacts
+//   }
+//   addContact = newContact => {
+//     this.setState(prevState => ({ showForm: false, contacts: [...prevState.contacts, newContact] }))
+//   }
+//   toggleContacts = () => {
+//     this.setState(prevState => ({ showContacts: !prevState.showContacts }))
+//   }
+
+//   toggleForm = () => {
+//     this.setState(prevState => ({ showForm: !prevState.showForm }))
+//   }
+
+//   showForm = () => {
+//     this.setState({ showForm: true })
+//   }
+
+//   // Fix array so that props change, so sort reloads page
+//   sort = () => {
+//     this.setState(prevState => ({
+//       contacts: [...prevState.contacts].sort(compareNames)
+//     }))
+//   }
+
+//   render() {
+//     // console.error('full page alert')
+//     // console.warn('this is a less harsh warning')
+//     if (this.state.showForm) return <AddContactForm onSubmit={this.addContact} />
+//     return (
+//       <View style={styles.container}>
+
+//         <Button title="Toggle Contacts" onPress={this.toggleContacts} />
+
+//         <Button title="Add Contact" onPress={this.showForm} />
+
+//         {this.state.showContacts && <ContactsList contacts={this.state.contacts} />}
+//       </View>
+//     )
+
+
+//   }
+// }
