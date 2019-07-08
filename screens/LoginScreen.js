41a9324c942eb8da5660523c2b39ad2c8f08a751
TammyLaforest <TammyLaforest@gmail.com>
2019-07-08 14:23:19 -0400

    continuing login page auth

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,15 +1,42 @@
 import React from "react";
-import { Button, View, StyleSheet, Text } from "react-native";
+import { Button, View, StyleSheet, TextInput } from "react-native";
 
 export default class LoginScreen extends React.Component {
+    state = {
+        username: "",
+        password: "",
+    }
+
     _login = () => {
-        this.props.navigation.navigate("Main");
-    };
+
+        fetch('http:://localhost:8000', {
+            method: 'POST',
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify({ username: this.state.username, password: this.state.password }),
+
+        }).then(res => console.log(res))
+        this.props.navigation.navigate("Main")
+    }
+
+    handleUsernameUpdate = username => {
+        this.setState({ username })
+    }
+    handlePasswordUpdate = password => {
+        this.setState({ password })
+    }
 
     render() {
         return (
             <View style={styles.container}>
-                <Text style={styles.text}>You are currently logged out.</Text>
+                {/* <Text style={styles.text}>You are currently logged out.</Text> */}
+                <TextInput
+                    placeholder='username'
+                    value={this.state.username}
+                    onChangeText={this.handleUsernameUpdate}
+                    autoCapitalize="none" />
+                <TextInput placeholder='password'
+                    value={this.state.password}
+                    onChangeText={this.handlePasswordUpdate} />
                 <Button title="Press to Log In" onPress={this._login} />
             </View>
         );
