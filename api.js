73ae4a8ad094377e1c591459cda13ebcb01eddf1
TammyLaforest <TammyLaforest@gmail.com>
2019-07-08 13:32:11 -0400

    Api working

--- api.js
+++ api.js
@@ -1,6 +1,13 @@
+
+const processContact = contact => ({
+    name: `${contact.name.first} ${contact.name.last}`,
+    phone: contact.phone
+})
+
 export const fetchUsers = async () => {
     const response = await fetch('https://randomuser.me/api?results=50&nat=us')
     const { results } = await response.json()
     console.log(results)
-    return results
+    return results.map(contact => processContact(contact))
+
 }
