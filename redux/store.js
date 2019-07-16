61bdaf8beb7233b1e16fd686667b5381097cdf56
TammyLaforest <TammyLaforest@gmail.com>
2019-07-16 11:54:36 -0400

    adding middleware thunk to project

--- redux/store.js
+++ redux/store.js
@@ -1,12 +1,28 @@
-import { createStore } from 'redux'
-import reducer from './reducer'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+
 import { addContact } from './actions'
-const store = createStore(reducer)
-export default store
+import reducer from './reducer'
+
+// Way to wrap an expression to delay its evaluation
+// const thunk = store = next => action => {
+//     if (typeof action === 'function') {
+//         action(store.dispatch)
+//     } else {
+//         next(action)
+//     }
+// }
+
+const store = createStore(reducer, applyMiddleware(thunk))
+
+
 
 // store.dispatch(updateUser({ foo: 'foo' }))
 // store.dispatch(updateUser({ bar: 'bar' }))
 // store.dispatch(updateUser({ foo: 'baz' }))
 
 store.dispatch(addContact({ name: 'Tammy', phone: '1231231234' }))
-store.dispatch(addContact({ name: 'Jenny', phone: '5435435432' }))
+
+
+console.log(store.getState())
+export default store
